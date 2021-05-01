18f16d926430d4a2b0895edb33117cd2b1257e3c
Pathompol.Nilchaikovit <data.noob.lol@gmail.com>
2021-05-01 23:30:56 +0700

    release verson v0.02

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'ggwp',
   packages = ['ggwp'],
-  version = '0.04',
+  version = '0.02',
   license='MIT',
   description = 'Prepare Fast, Analyze Faster',
   author = 'pathompol',
