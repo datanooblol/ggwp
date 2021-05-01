e17e577780ebc282479939d55b24be7e3d7b2476
Pathompol.Nilchaikovit <data.noob.lol@gmail.com>
2021-05-01 23:48:54 +0700

    v0.04 imort issued from ggwp.file import class

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'ggwp',
   packages = ['ggwp'],
-  version = '0.03',
+  version = '0.04',
   license='MIT',
   description = 'Prepare Fast, Analyze Faster',
   author = 'pathompol',
   author_email = 'data.noob.lol@gmail.com',
   url = 'https://github.com/datanooblol/ggwp',
   # check url everytime you release new version
-  download_url = 'https://github.com/datanooblol/ggwp/archive/refs/tags/0.03.tar.gz',
+  download_url = 'https://github.com/datanooblol/ggwp/archive/refs/tags/0.04.tar.gz',
   keywords = ['ez','rfm','prep', 'data model', 'cohort', 'customer'],
   # dependencies used in your library
   install_requires=[
