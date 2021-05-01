a76d72fa5e95e6d6132afa9e537cbf4a378e3456
Pathompol.Nilchaikovit <data.noob.lol@gmail.com>
2021-05-02 00:57:46 +0700

    v0.09 import issue

--- ggwp/__init__.py
+++ ggwp/__init__.py
@@ -1,8 +1,6 @@
 # from folder.file import class
-from .datamodel import DataModel
-from .ezcheck import EzCheck
-from .ezcohort import EzCohort
-from .ezcustomermovement import EzCustomerMovement
-from .ezrfm import EzRFM
-
-# from * import DataModel, EzCheck, EzCohort, EzCustomerMovement, EzRFM
+from .DataModel import DataModel
+from .EzCheck import EzCheck
+from .EzCohort import EzCohort
+from .EzCustomerMovement import EzCustomerMovement
+from .EzRFM import EzRFM
