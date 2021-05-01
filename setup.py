0bae0b6a401e776a826d6ded5dacc0acaaf18a80
Pathompol.Nilchaikovit <data.noob.lol@gmail.com>
2021-05-02 02:07:53 +0700

    alpha.0.0.1

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
   name = 'ggwp',
   packages = ['ggwp'],
-  version = '0.11',
+  version = 'alpha.0.0.1',
   license='MIT',
   description = 'Prepare Fast, Analyze Faster',
+  long_description = long_description,
   author = 'pathompol',
   author_email = 'data.noob.lol@gmail.com',
   url = 'https://github.com/datanooblol/ggwp',
   # check url everytime you release new version
-  download_url = 'https://github.com/datanooblol/ggwp/archive/refs/tags/0.11.tar.gz',
+  download_url = 'https://github.com/datanooblol/ggwp/archive/refs/tags/alpha.0.0.1.tar.gz',
   keywords = ['ez','rfm','prep', 'data model', 'cohort', 'customer'],
   # dependencies used in your library
   install_requires=[
@@ -21,7 +29,7 @@ setup(
     #   3 - Alpha
     #   4 - Beta
     #   5 - Production/Stable
-    'Development Status :: 4 - Beta',
+    'Development Status :: 3 - Alpha',
 
     # Indicate who your project is intended for
     'Intended Audience :: Developers',
