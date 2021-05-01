5d0eccf992a4f7ad9f0c4e4f4af96519f2ce177e
Pathompol.Nilchaikovit <data.noob.lol@gmail.com>
2021-05-01 22:40:49 +0700

    v0.02  fix import problem

--- ggwp/__init__.py
+++ ggwp/__init__.py
@@ -1,6 +1,6 @@
 # from folder.file import class
-from ggwp.DataModel import DataModel
-from ggwp.EzCheck import EzCheck
-from ggwp.EzCohort import EzCohort
-from ggwp.EzCustomerMovement import EzCustomerMovement
-from ggwp.EzRFM import EzRFM
+from . import DataModel
+from . import EzCheck
+from . import EzCohort
+from . import EzCustomerMovement
+from . import EzRFM
