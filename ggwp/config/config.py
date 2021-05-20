9d872e755da32a5e63d3a20cf0c8e2fafd4820f9
bank <data.noob.lol@gmail.com>
2021-05-20 10:45:32 +0700

    v0.0.47 revised codes

--- ggwp/config/config.py
+++ ggwp/config/config.py
@@ -1,18 +1,3 @@
-# import ggwp, os
-# import configparser
-
-# current = os.sep.join(ggwp.__file__.split(os.sep)[:-1])
-# config_path  = '{0}/config/config.cfg'.format(current)
-# parser = configparser.ConfigParser()
-# parser.read(config_path)
-# __version__ = parser['default']['version']
-
-
-# class EzConfig:
-
-#     def __init__(self):
-#         self.__version__ = __version__
-
 import ggwp
 import os
 import configparser
